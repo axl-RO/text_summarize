94403551daa884cd6f926480f98b68a43f0f0c05
hrutuj <hrutujturankar1@gmail.com>
2025-12-29 12:40:16 +0530

    FLAN model implementation

--- summarizer.py
+++ summarizer.py
@@ -1,6 +1,6 @@
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 
-MODEL_NAME = "t5-small"
+MODEL_NAME = "google/flan-t5-small"
 
 tokenizer = T5Tokenizer.from_pretrained(MODEL_NAME)
 model = T5ForConditionalGeneration.from_pretrained(MODEL_NAME)
